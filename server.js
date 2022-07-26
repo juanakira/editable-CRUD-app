cac451f95d84dc6dbc82840604840906c2175db1
coscorrodrift <juan.dyamamoto@gmail.com>
2022-07-26 23:30:37 +0200

    heroku 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const dotenv = require("dotenv");
 dotenv.config()
 const mongooseModel = require('./models/myModel');
